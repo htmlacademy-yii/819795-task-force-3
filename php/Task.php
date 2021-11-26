3e24ce4930e0be1e844587a4868365e6d4cea49f
Basay <Basay1980@gmail.com>
2021-11-26 16:49:59 +0200

    сделал что мог..как сделать тестирование не понял

---
+++ php/Task.php
@@ -0,0 +1,104 @@
+<?php
+
+class Task
+{
+  private $executor;
+
+  private $customer;
+
+  private $status;
+
+
+  const STATUS_NEW = 'new';
+  const STATUS_CANCEL = 'cancel';
+  const STATUS_IN_WORK = 'inwork';
+  const STATUS_DONE = 'done';
+  const STATUS_FAIL = 'failed';
+
+
+  const ACTION_CREATE = 'action_create';
+  const ACTION_CANCEL = 'action_cancel';
+  const ACTION_RESPOND = 'action_respond';
+  const ACTION_REFUSE = 'action_refuse';
+  const ACTION_DONE = 'action_done';
+
+  const STATUSES_ARRAY = [
+          self::STATUS_NEW => 'Новый',
+          self::STATUS_CANCEL => 'Отменено',
+          self::STATUS_IN_WORK => 'В работе',
+          self::STATUS_DONE => 'Выполнено',
+          self::STATUS_FAIL => 'Провалено',
+      ];
+
+  CONST ACTIONS_ARRAY = [
+         self::ACTION_CREATE => 'Создать',
+         self::ACTION_CANCEL => 'Отменить',
+         self::ACTION_RESPOND => 'Откликнуться',
+         self::ACTION_REFUSE => 'Отказаться',
+         self::ACTION_DONE => 'Выполнено',
+  ];
+
+  public function getStatusesMap ()
+  {
+      return self::STATUSES_ARRAY;
+  }
+
+  public function getActionsMap ()
+  {
+      return self::ACTIONS_ARRAY;
+  }
+
+
+  public function setCustomer ($str)
+  {
+      $this->customer = $str;
+  }
+
+  public function setExecutor ($str)
+  {
+        $this->executor = $str;
+  }
+
+  public function setStatus ($str)
+  {
+        $this->status = self::STATUSES_ARRAY[$str];
+  }
+
+  public function __construct($customer, $status = 'new', $executor = null)
+  {
+        $this->setCustomer($customer);
+        $this->setExecutor($executor);
+        $this->setStatus($status);
+  }
+
+  public function getNextStatus($action)
+  {
+       switch ($action) {
+            case self::ACTION_CREATE:
+                return self::STATUS_NEW;
+            case self::ACTION_CANCEL:
+                return self::STATUS_CANCEL;
+            case self::ACTION_DONE:
+                return self::STATUS_DONE;
+            case self::ACTION_RESPOND:
+                return self::STATUS_IN_WORK;
+           case self::ACTION_REFUSE:
+               return self::STATUS_FAIL;
+        }
+        return null;
+  }
+
+  public function getAllowedAction() : array
+   {
+        switch ($this->status) {
+            case(self::STATUS_NEW):
+                return [self::ACTION_CANCEL, self::ACTION_RESPOND];
+            case(self::STATUS_IN_WORK):
+                return [self::ACTION_DONE, self::ACTION_REFUSE];
+        }
+        return [];
+   }
+
+
+}
+
---
+++ php/test.php
@@ -0,0 +1,11 @@
+<?php
+
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+assert_options(ASSERT_QUIET_EVAL, 1);
+
+include 'Task.php';
+
+$task = new Task(1);
+
+assert($task->getNextStatus('action_respond') == Task::STATUS_NEW, 'respond action');
